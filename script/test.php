d2d4650aa76e32dbb33318ffca47e4ce0ba09cb5
Christian Seidlitz <Christian.Seidlitz@gmx.de>
2014-12-19 15:20:18 +0100

    updated test-file

--- script/test.php
+++ script/test.php
@@ -1,28 +1,12 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Vittel
+ * User: Christian Seidlitz
  * Date: 19.12.2014
  * Time: 14:38
  */
 
-// Your custom class dir
-define('CLASS_DIR', realpath('../src'));
-
-// Add your class dir to include path
-set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
-
-// You can use this trick to make autoloader look for commonly used "My.class.php" type filenames
-spl_autoload_extensions('.php');
-
-// Use default autoload implementation
+set_include_path(realpath('../src'));
 spl_autoload_register();
 
-try
-{
-    \RumbleTeam\BBCodeParser\Token\Token::tokenizeDirect('alf [alal] test');
-}
-catch (\Exception $ex)
-{
-    var_dump($ex);
-}
+\RumbleTeam\BBCodeParser\Token\Token::tokenizeDirect('alf [alal] test');
